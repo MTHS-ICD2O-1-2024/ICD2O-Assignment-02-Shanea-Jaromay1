f926a76b699232b07f62e1f7ceb2185aef161048
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-19 23:26:24 +0000

    code changed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Show volume
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const radiusNumber = document.getElementById("radius-number").value
+  const heightNumber = document.getElementById("height-number").value
+  // output
+  document.getElementById("volume").innerHTML =
+    "Your volume is: " + radiusNumber + ", " + heightNumber + "."
 }
